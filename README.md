07b0112e45d5c0388baccbef30ba3cfdf207d79d
street62 <tmdfl1027@gmail.com>
2021-09-22 11:19:23 +0900

    middlewares 정리/URL 관련 README 추가

---
+++ README.md
@@ -0,0 +1,14 @@
+# wetube reloaded
+
+/ -> Home
+/login -> login
+/search -> search
+
+/users/edit -> Edit Profile
+/users/delete -> Delete Profile
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/video/comments -> Leave a Comment
+/video/comments/delete -> Delete a Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,37 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 const handleListening = () => console.log(`Server listening on http://localhost:${PORT} 🚀`)
+
 const handleLogin = (req, res) => {
   res.send("login here!");
 }
-
-const loggerMiddleware = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-}
-const protectionMiddleware = (req, res, next) => {
-  const url = req.url;
-  if(url === "/protected") {
-    return res.send("<h1>Not Allowed!</h1>");
-  }
-  console.log("Allowed. You may continue.");
-  next();
-}
-const handleHome = (req, res, next) => {
-  return res.send("I love middlewares!");
+const handleHome = (req, res) => {
+  return res.send("<h1>Hello!</h1>");
 }
 
-const handleProtected = (req, res, next) => {
-  return res.send("Welcome to the private lounge");
-}
-
-app.use(loggerMiddleware);
-app.use(protectionMiddleware);
+app.use(logger);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
-app.get("/protected", handleProtected);
 
 app.listen(PORT, handleListening);
